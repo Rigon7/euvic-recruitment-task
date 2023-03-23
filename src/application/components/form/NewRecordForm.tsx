005379d5606346aa2c40103a506373ea18a40e15
Dominik Olbrycht <dominik.olbrycht@gmail.com>
2023-03-23 20:56:28 +0100

    add modal form

--- src/application/components/form/NewRecordForm.tsx
+++ src/application/components/form/NewRecordForm.tsx
@@ -1,24 +1,53 @@
 import { useTranslation } from 'react-i18next';
 import Button from '@mui/material/Button';
 import { MouseEventHandler } from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+
+interface NewRecordFormData {
+    firstName: string;
+    age: number;
+    birthDate: string;
+    bio: string;
+}
 
 const NewRecordForm = (props: { handleClose: MouseEventHandler<HTMLButtonElement> | undefined }): JSX.Element => {
     const { t } = useTranslation();
 
-    const { register, handleSubmit } = useForm();
+    const schema = yup.object().shape({
+        firstName: yup.string().required(t('errorFirstNameRequired') ?? ''),
+        age: yup
+            .number()
+            .required(t('errorFirstNameRequired1') ?? '')
+            .positive()
+            .integer()
+            .min(1),
+        birthDate: yup.date().required(t('errorFirstNameRequired2') ?? ''),
+        bio: yup.string().max(250)
+    });
 
-    const onSubmit = (data: any): void => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<NewRecordFormData>({
+        resolver: yupResolver(schema)
+    });
+
+    const onSubmit = (data: NewRecordFormData): void => {
         console.log(data);
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder={t('firstName') ?? ''} {...register('firstName')} />
+            <Typography>{errors.firstName?.message?.toString()}</Typography>
             <input type="number" placeholder={t('age') ?? ''} {...register('age')} />
-            <input type="date" {...register('date')} />
+            <Typography>{errors.age?.message?.toString()}</Typography>
+            <input type="date" {...register('birthDate')} />
+            <Typography>{errors.birthDate?.message?.toString()}</Typography>
             <input type="text" placeholder={t('bio') ?? ''} {...register('bio')} />
             <Box>
                 <Button type="submit">{t('save')}</Button>
