cdbad2c253f72c4225b249412bb8c3aace49fe28
Dominik <dominik.olbrycht@gmail.com>
2023-04-28 11:48:11 +0200

    github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './src/App';
+import App from './App';
 import './index.scss';
-import './src/i18';
+import './i18';
 
 import { Provider } from 'react-redux';
-import reportWebVitals from './src/reportWebVitals';
-import store from './src/application/redux/store/store';
+import reportWebVitals from './reportWebVitals';
+import store from './application/redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
