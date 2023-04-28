6cd7d51e93594475c1cea0d99dc3c121c78eb9fa
Dominik <dominik.olbrycht@gmail.com>
2023-04-28 11:51:31 +0200

    change index.tsx directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from '../App';
+import App from './App';
 import './index.scss';
-import '../i18';
+import './i18';
 
 import { Provider } from 'react-redux';
-import reportWebVitals from '../reportWebVitals';
-import store from './redux/store/store';
+import reportWebVitals from './reportWebVitals';
+import store from './application/redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
